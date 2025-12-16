a1eb2e7e26e9e0aa8047dee12c9fb19761f8caac
Anton Shevchenko <avshev4enko@gmail.com>
2025-12-16 10:25:47 +0000

    Make accounts clickable and fix accounts show page

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,8 +1,22 @@
 class AccountsController < ApplicationController
-  before_action :set_account, only: [:edit, :update, :destroy]
+  before_action :set_account, only: [:show, :edit, :update, :destroy]
 
   def index
     @accounts = Current.user.accounts.order(created_at: :desc)
+  end
+
+  def show
+    # Parse defi_positions JSON for view
+    @defi_positions = parse_defi_positions(@account.defi_positions)
+
+    # Calculate portfolio percentages
+    if @account.balance_usd.to_f > 0
+      @wallet_percentage = (@account.wallet_balance_usd.to_f / @account.balance_usd.to_f * 100).round(1)
+      @protocol_percentage = (@account.protocol_balance_usd.to_f / @account.balance_usd.to_f * 100).round(1)
+    else
+      @wallet_percentage = 0
+      @protocol_percentage = 0
+    end
   end
 
   def new
@@ -43,5 +57,24 @@ class AccountsController < ApplicationController
 
   def account_params
     params.require(:account).permit(:address, :name, :group, :description, :status)
+  end
+
+  def parse_defi_positions(defi_positions)
+    return [] if defi_positions.blank?
+
+    # If it's already an Array, return as-is
+    return defi_positions if defi_positions.is_a?(Array)
+
+    # If it's a String, parse the JSON
+    if defi_positions.is_a?(String)
+      begin
+        JSON.parse(defi_positions)
+      rescue JSON::ParserError
+        []
+      end
+    else
+      # If it's some other type (Hash, etc.), convert to array
+      Array(defi_positions)
+    end
   end
 end
